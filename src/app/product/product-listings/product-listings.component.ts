2f489e9049b3439328e08535f90ee60f2134a61d
ukkiy-ray <showy13f207ag@gmail.com>
2021-07-24 17:58:46 +0900

    Angular フロントエンド作成

---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+import { products } from '../../products';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss']
+})
+export class ProductListingsComponent implements OnInit {
+
+  products: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.products = products
+  }
+
+}
---
+++ src/app/products.ts
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    text1: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    text2: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+    text3: 'onec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.'
+  },
+  {
+    coverImage: './assets/img/pen-and-phone.jpg',
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    text1: 'サンプルテキスト1',
+    text2: 'サンプルテキスト2',
+    text3: 'サンプルテキスト3'
+  },
+  {
+    coverImage: './assets/img/laptop.jpg',
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    text1: 'サンプルテキスト1',
+    text2: 'サンプルテキスト2',
+    text3: 'サンプルテキスト3'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone 12 Pro Max',
+    price: 2500,
+    description: '',
+    heading1: 'サンプルテキスト1',
+    heading2: 'サンプルテキスト2',
+    heading3: 'サンプルテキスト3',
+    text1: 'サンプルテキスト1',
+    text2: 'サンプルテキスト2',
+    text3: 'サンプルテキスト3'
+  }
+];
