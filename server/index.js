736d60a2c77a36bdc75205efa3c6b1e0a2600d28
ukkiy-ray <showy13f207ag@gmail.com>
2021-07-25 03:05:46 +0900

    Nodeサーバー構築

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/dev');
+const FakeDb = require('./fake-db');
+
+const productsRoutes = require('./routes/products');
+
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(
+  () => {
+    const fakeDb = new FakeDb();
+    fakeDb.initDb();
+  }
+);
+
+const app = express();
+
+app.use('/api/v1/products', productsRoutes);
+
+
+const PORT = process.env.PORT || '3001';
+
+app.listen(PORT, function() {
+  console.log('server is running! [ port: 3001 ]');
+});
